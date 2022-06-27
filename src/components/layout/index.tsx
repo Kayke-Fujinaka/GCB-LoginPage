a40392a59f0f0159a9164ed201e98419a6bb347d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-06-27 13:41:59 -0300

    [Feat] Create Layout Component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
+import Head from "next/head";
 
-export default function Layout() {
-  return <div>Layout</div>;
+interface LayoutProps {
+  children: React.ReactNode;
+  title: string;
+  description: string;
+  url: string;
 }
 
+export function Layout({ children, title, description, url }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>GCB Investimentos - {title}</title>
+        <meta name="description" content={description + " Page"} />
+        <meta property="og:title" content="GCB Investimentos" />
+        <meta property="og:description" content={description + " Page"} />
+        <meta property="og:url" content={"http://localhost:3000/" + url} />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>{children}</div>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
 import React from "react";
-import Head from "next/head";
+import { Layout } from "../components/layout";
 
 export default function Login() {
   return (
     <>
-      <Head>
-        <title>GCB Investimentos</title>
-        <meta name="description" content="A Grupo GCB Login Page" />
-        <meta property="og:title" content="GCB Investimentos" />
-        <meta property="og:description" content="A Grupo GCB Login Page" />
-        <meta property="og:url" content="http://localhost:3000" />
-        <meta property="og:type" content="website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>Login</div>
+      <Layout title='Login' description='Login' url="">
+        <div>Oi</div>
+      </Layout>
     </>
   );
 }
