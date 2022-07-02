15f7c319908e09a705ec5df511b34617d36b27ae
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-02 15:37:05 -0300

    [Feat] Create Content Storybook

---
+++ src/components/Content/Content.stories.tsx
@@ -0,0 +1,11 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Content } from ".";
+
+export default {
+  title: "Contents/Content",
+  component: Content,
+} as ComponentMeta<typeof Content>;
+
+const Template: ComponentStory<typeof Content> = (args) => <Content />;
+
+export const ContentDesktop = Template.bind({});
