d122b3d7dd102560c39ca0e6660dbc1f5d49e458
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-07-13 14:33:41 -0300

    [Feat] Jest in Token Generator

--- src/utils/tokenGenerator.ts
+++ src/utils/tokenGenerator.ts
@@ -1,7 +1,7 @@
 export const generateToken = (lengthChars: number) => {
-  var CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  var tokenStore = "";
-  for (var i = 0; i < lengthChars; i++) {
+  const CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let tokenStore = "";
+  for (let i = 0; i < lengthChars; i++) {
     tokenStore += CHARS[Math.floor(Math.random() * CHARS.length)];
   }
   return tokenStore;
